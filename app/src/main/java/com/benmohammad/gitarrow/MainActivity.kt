32a5344647fccafa30d3d7f8ba0232973739a103
Ben Mohammad <benmohammad593@gmail.com>
2020-07-04 18:10:13 +0100

    MainActivity added...Manifest bug fix...

--- app/src/main/java/com/benmohammad/gitarrow/MainActivity.kt
+++ app/src/main/java/com/benmohammad/gitarrow/MainActivity.kt
@@ -2,11 +2,25 @@ package com.benmohammad.gitarrow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.benmohammad.gitarrow.repo.RepoFragment
+import com.benmohammad.gitarrow.repo.RepoPresenter
+import com.benmohammad.gitarrow.util.Injection
+import com.benmohammad.gitarrow.util.replaceFragmentInActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var repoPresenter: RepoPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val repoFragment = supportFragmentManager.findFragmentById(R.id.contentFrame) as RepoFragment? ?: RepoFragment.newInstance().also {
+            replaceFragmentInActivity(it, R.id.contentFrame)
+        }
+
+        repoPresenter = RepoPresenter(Injection.provideRepoRepository(applicationContext), repoFragment)
     }
 }
